60da3ac93680295a165842452715ade9bc5e4b9b
WetSock <imperatorofmagic@gmail.com>
2016-12-15 01:15:45 +0200

    Добавил основной файл проекта
    Смог получить данные о названии фильма, актерах и описание

---
+++ ViewController.swift
@@ -0,0 +1,66 @@
+//
+//  ViewController.swift
+//  NSURLSession
+//
+//  Created by Анонимко on 11/12/16.
+//  Copyright © 2016 NonameOrg. All rights reserved.
+//
+
+import UIKit
+import Kanna
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var myWebView: UIWebView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        displayURL()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    func displayURL(){
+        let myURLAdress = "https://www.kinopoisk.ru/film/9028/"
+        let myURL = URL(string: myURLAdress)
+        let URLTask = URLSession.shared.dataTask(with: myURL!, completionHandler: {
+            myData, response, error in
+            
+            guard error == nil else {return}
+            
+            let myHTMLString = String(data: myData!, encoding: String.Encoding.windowsCP1251)
+            
+       //     self.myWebView.loadHTMLString(myHTMLString!, baseURL: nil)
+            
+            
+            
+            if let doc = HTML(html: myHTMLString!, encoding: .windowsCP1251) {
+                // print(doc.title)
+                
+                // Search for nodes by CSS
+//                for link in doc.css("a, link") {
+//                    print(link.text)
+//                    print(link["href"])
+//                }
+                
+                // Search for nodes by XPath
+                for link in doc.xpath("//h1[@itemprop='name']") {
+                    print(link.text)
+                    //print(link["href"])
+                }
+                for link in doc.xpath("//li[@itemprop='actors']") {
+                    print(link.text)
+                    //print(link["href"])
+                }
+                for link in doc.xpath("//div[@itemprop='description']") {
+                    print(link.text)
+                    //print(link["href"])
+                }
+            }
+        })
+        URLTask.resume()
+        
+    }
+    
+
+}
+
